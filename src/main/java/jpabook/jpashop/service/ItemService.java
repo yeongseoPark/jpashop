227ba09eeeacda20ff486bf433fc10cedc835cc6
YSEO <1park4170@gmail.com>
2022-08-23 12:53:24 +0900

    ~web finished

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Item;
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional /* 변경 감지 기능 사용 */
+    public Item updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId); // 영속상태
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        // 값을 세팅후 Transactional에 의해 트랜잭션이 커밋됨 -> JPA가 flush
+        // 이때 영속성 컨테스트의 바뀐값에 대해 update가 날아간다
+        return findItem;
     }
 
     public List<Item> findItems() {
--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -53,6 +53,7 @@ public class OrderService {
 
     // 검색
     public List<Order> findOrders(OrderSearch orderSearch) {
+
         return orderRepository.findAll(orderSearch);
     }
 }
